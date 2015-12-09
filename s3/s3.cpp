7045e7ac1571f7cf25ffc76b017bb38946d1667b
Jim Mao <jim.mao@tufts.edu>
2015-12-09 01:37:08 -0500

    working get and put

--- s3/s3.cpp
+++ s3/s3.cpp
@@ -7,10 +7,21 @@
 int main() {
 
 	// Any ostream will work here
-	std::ofstream out;
+	std::ofstream out ("sample.txt");
 
-	AWS::S3::Connection s3("ACCESS_KEY_ID", "SECRET_ACCESS_KEY");
+	AWS::S3::Connection s3("","");
 	if (s3.get("test-flyingsquid", "/sample.txt", out)) {
+	    std::cout << "Success!" << std::endl;
+	} else {
+	    std::cout << "Failure" << std::endl;
+	}
+
+
+	std::ifstream in ("blah.txt", std::ifstream::in);
+	std::ofstream output ("foo.txt");
+	std::size_t N = 10;
+
+	if (s3.put("test-flyingsquid", "/blah.txt", in, 11, output, N)) {
 	    std::cout << "Success!" << std::endl;
 	} else {
 	    std::cout << "Failure" << std::endl;
